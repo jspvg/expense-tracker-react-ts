9aa49149497b9763cd02c79790b8006b8e8642b7
jspvg <gabrijelajosipovic1@gmail.com>
2024-02-25 14:01:52 +0100

    create new login and register page scaffold

--- src/pages/register/index.tsx
+++ src/pages/register/index.tsx
@@ -1,32 +1,28 @@
-import { useNavigate, Navigate } from "react-router-dom";
-import { useGetUserInfo } from "../../hooks/useGetUserInfo";
+import { useNavigate } from "react-router-dom";
 import { signInWithGoogle } from "../../utils/firebase/google";
 
-const Auth = () => {
+const Register = () => {
   const navigate = useNavigate();
-  const { isAuth } = useGetUserInfo();
-
   const errortext = "";
-
-  if (isAuth) {
-    return <Navigate to="expense-tracker" />;
-  }
-
   return (
-    <form action="submit" id="login">
-      <h3>Login</h3>
+    <form action="submit" id="register">
+      <h3>Register account</h3>
       <label htmlFor="email">email</label>
       <input type="email" id="email" />
       <p className="error">{errortext && errortext}</p>
       <label htmlFor="password">password</label>
       <input type="password" id="password" />
       <p className="error">{errortext && errortext}</p>
-      <button>login</button>
+      <label htmlFor="password">repeat password</label>
+      <input type="password" id="password-repeat" />
+      <p className="error">{errortext && errortext}</p>
+
+      <button>register</button>
       <button onClick={() => signInWithGoogle({ navigate })}>
-       or sign in with Google
+        or sign in with Google
       </button>
     </form>
   );
 };
 
-export default Auth;
+export default Register;
